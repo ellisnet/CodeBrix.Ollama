a79d58b5efa35d96cfb2891134e18c14d72a2241
Jeremy Ellis <jeremy@ellisnet.com>
2026-09-16 00:35:08 -0700

    Add ModelQueryTool sample.

---
+++ samples/ModelQueryTool/tests/libs/ModelQueryTool.ModelRunning.Tests/BasicTests.cs
@@ -0,0 +1,17 @@
+using SilverAssertions;
+using Xunit;
+
+namespace ModelQueryTool.ModelRunning.Tests;
+
+public class BasicTests
+{
+    [Fact]
+    public void can_run_tests()
+    {
+        //Arrange
+        var isRunning = true;
+
+        //Assert
+        isRunning.Should().Be(true);
+    }
+}
